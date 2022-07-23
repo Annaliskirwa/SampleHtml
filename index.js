41c34e9b6bbabf80b2e7c76432b3f8dc2cdf8b03
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-23 23:33:57 +0300

    Added js to add the nums

---
+++ index.js
@@ -0,0 +1,6 @@
+function add(){
+    let firstNum = parseInt(document.getElementById("number").value);
+    let secondNum = parseInt(document.getElementById("number1").value);
+
+    alert(firstNum + secondNum)
+}
