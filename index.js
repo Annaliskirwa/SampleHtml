cba37048187fb66dd4390588ef9a78b5086874df
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-23 23:50:20 +0300

    Corrected to id, add, if statement

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ function add(){
     let firstNum = parseInt(document.getElementById("number").value);
     let secondNum = parseInt(document.getElementById("number1").value);
 
-    alert(firstNum + secondNum)
+   console.log(typeof(firstNum))
+//    alert(firstNum + secondNum)
+
+     if (typeof(firstNum) && typeof(secondNum) != null){
+        alert(firstNum + secondNum)
+       
+     }
+     else{
+          
+        alert("Insert a number")  
+    }
 }
