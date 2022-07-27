3d4cc9b0dc6e52d9b2ad4fb3f83826c6e9db4064
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-27 22:14:04 +0300

    Dispalying results from api

--- statesexamples/src/components/useeffect.jsx
+++ statesexamples/src/components/useeffect.jsx
@@ -1,17 +1,21 @@
 import React , {useEffect} from "react";
 
 import axios from "axios";
+import { useState } from "react";
 
 function EffectExample(){
+    const [data, setData] = useState("");
     useEffect(()=>{
         // console.log("Hello worlddddd");
         axios.get("https://jsonplaceholder.typicode.com/comments").then((response)=>{
-            console.log(response.data);
+            setData(response.data[0].email);
+            // console.log(response.data);
+            console.log("Calling the api how many times")
         })
     })
     return (
         <div>
-            <p>Hello worrrlllddd</p>
+            <p>Hello worrrlllddd {data}</p>
         </div>
     )
 }
