ddd2c0f6e3870d4cea13c81037b514b09dad35d2
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-27 21:21:54 +0300

    Added react states

--- statesexamples/src/components/usestate.jsx
+++ statesexamples/src/components/usestate.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { useState } from "react";
 
 const StateExample = () =>{
-    let counter = 0;
+    // let counter = 0;
+    const [counter, setCounter] = useState(0);
     const increment = ()=>{
-        counter = counter + 1;
-        console.log(counter);
+        // counter = counter + 1;
+        // console.log(counter);
+
+        setCounter(3);
     }
     return (
         <div>
+            <p>{counter}</p>
             <button onClick={increment}>Increment</button>
         </div>
     )
