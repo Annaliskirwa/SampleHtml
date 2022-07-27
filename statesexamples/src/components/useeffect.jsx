1884be54c4b1edf5a16e37073d8b5893a04fadb6
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-27 22:11:17 +0300

    Hit an api endpoint, log data on page render

--- statesexamples/src/components/useeffect.jsx
+++ statesexamples/src/components/useeffect.jsx
@@ -1,8 +1,13 @@
 import React , {useEffect} from "react";
 
+import axios from "axios";
+
 function EffectExample(){
     useEffect(()=>{
-        console.log("Hello worlddddd");
+        // console.log("Hello worlddddd");
+        axios.get("https://jsonplaceholder.typicode.com/comments").then((response)=>{
+            console.log(response.data);
+        })
     })
     return (
         <div>
