0e24efaac0c4140ce297a53c14f47e0c0056344a
Annaliskirwa <annaliskirwa@gmail.com>
2022-07-27 21:29:36 +0300

    Use input to set the input values

--- statesexamples/src/components/usestate1.jsx
+++ statesexamples/src/components/usestate1.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { useState } from "react";
 const StateTutorial = ()=>{
     const [inputValue, setInputValue] = useState("Ann");
+    let onChange = (event) => {
+        const newValue = event.target.value;
+        setInputValue(newValue);
+    }
     return(
         <div>
             <input placeholder="Enter your name"></input>
