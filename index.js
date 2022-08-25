89edddfba6103edd401898eed510ec5461f5eeef
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-25 20:23:18 +0300

    String manipulations in javascript

--- index.js
+++ index.js
@@ -42,3 +42,29 @@ function playFootball(){
    console.log(players);
 }
 console.log("Anna is trying something")
+
+function stringManipulation(){
+   let word = "Learning Js";
+   let numberPhone = "07 02 91 45"
+   console.log(word.length);
+   // we count from 0
+   console.log(word.charAt(1));
+   // removes trailing and leading white spaces
+   console.log(word.trim());
+   // returns true or false 
+   console.log(word.includes("Learning"));
+   console.log(word.includes("Anna"));
+
+      // regular expression
+   // d n  ^ a 
+   let regexOurs = /^[a-zA-Z]+$/
+   // null
+   console.log(word.match(regexOurs));
+   // old string, new string
+   console.log(word.replace("Learning", "Listening"));
+   console.log(word.split(" "));
+   // returns from the first, doesnt return that last index
+   console.log(word.substring(0,4));
+   console.log(word.toUpperCase());
+   console.log(word.toLowerCase());
+}
